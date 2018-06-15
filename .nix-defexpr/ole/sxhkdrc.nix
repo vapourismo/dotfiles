5f8e9914bf6b5394a780716ab47f8f55366497f5
Ole <ole@vprsm.de>
2018-06-15 18:45:09 +0100

    Fri 15 Jun 18:45:09 BST 2018

---
+++ .nix-defexpr/ole/sxhkdrc.nix
@@ -0,0 +1,23 @@
+{
+	stdenv,
+	writeText,
+
+	bindings ? []
+}:
+
+let mkBinding = {key, command}: key + "\n\t" + command;
+
+	contents = builtins.concatStringsSep "\n\n" (map mkBinding bindings);
+
+in stdenv.mkDerivation {
+	name = "sxhkdrc";
+
+	sxhkdrc = writeText "sxhkdrc" contents;
+
+	phases = ["installPhase"];
+
+	installPhase = ''
+		mkdir -p $out/etc/sxhkd
+		cp $sxhkdrc $out/etc/sxhkd/sxhkdrc
+	'';
+}
